b0c10c6c316a6ea9953d7f0aba26871135713476
saivikassingamsetty <saivikas.singamsetty@gmail.com>
2022-09-05 00:07:57 +0530

    array-day-4

--- Array/1.CheckArraySortedRotated.py
+++ Array/1.CheckArraySortedRotated.py
@@ -3,8 +3,19 @@ Given an array nums, return true if the array was originally sorted in non-decre
 then rotated some number of positions (including zero). Otherwise, return false.
 """
 def check(nums) -> bool:
-    sorted_nums = sorted(nums)
-    for i in range(len(nums)):
-        if nums[i+1:] + nums[:i+1] == sorted_nums:
-            return True
-    return False
+    #O(nlogn)
+    # sorted_nums = sorted(nums)
+    # for i in range(len(nums)):
+    #     if nums[i+1:] + nums[:i+1] == sorted_nums:
+    #         return True
+    # return False
+
+    #O(n)
+    k = 0
+    n = len(nums)
+    for i in range(n):
+        if nums[i] > nums[(i+1)%n]:
+            k += 1
+        if k > 1:
+            return 0
+    return 1
