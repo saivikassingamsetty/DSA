cf60b94e5ce81bbac1def6de459753d94df38b8b
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-08-11 20:51:06 +0530

    Time: 53 ms (58.22%), Space: 49.3 MB (20.31%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let res = [];
+    arr.forEach((value, index) => {
+        if (fn(value, index)) res.push(value);
+    });
+
+    return res;
+};
