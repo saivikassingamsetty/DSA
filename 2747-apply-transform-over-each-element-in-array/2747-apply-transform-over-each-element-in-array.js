885eac311a3f0f02a3542e201c673347d2d5d6a9
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-08-11 19:43:59 +0530

    Time: 53 ms (48.13%), Space: 49.5 MB (10.28%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let res = [];
+    arr.forEach((value, index) => {
+        res.push(fn(value, index))
+    });
+
+    return res;
+};
