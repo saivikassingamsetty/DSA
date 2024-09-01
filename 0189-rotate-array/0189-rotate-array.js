ed011c06910c0103e4eeeecddf634c0114a93422
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-09-01 12:45:44 +0530

    Time: 84 ms (45.29%), Space: 63.6 MB (5.22%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,10 +4,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    let numsCopy = [...nums];
-
-    for (let i=0; i<nums.length; i++) {
-        let mK = k%nums.length;
-        nums[i] = i >= mK ? numsCopy[i-mK]: numsCopy[nums.length+(i-mK)];
+    //reverse
+    nums.reverse();
+    let boundedK = k%nums.length;
+    //reverse first k
+    for (let i=0; i<Math.floor(boundedK/2); i++) {
+        [nums[i], nums[boundedK-1-i]] = [nums[boundedK-1-i], nums[i]];
+    }
+    //reverse last n-k
+    for (let i=0; i<Math.floor((nums.length-boundedK)/2); i++) {
+        [nums[i+boundedK], nums[nums.length-1-i]] = [nums[nums.length-1-i], nums[i+boundedK]];
     }
 };
