8b5243b9476f4605e509c4f73a1d80fd6749bb1f
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-09-02 19:05:18 +0530

    Time: 106 ms (63.68%), Space: 59.9 MB (29.72%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.js
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.js
@@ -4,5 +4,24 @@
  * @return {number[][]}
  */
 var sortTheStudents = function(score, k) {
-    return score.sort((a,b) => b[k] - a[k]);
+    //with JS in-built
+    // return score.sort((a,b) => b[k] - a[k]);
+
+    //Without in-built
+    for (let i = 0; i<score.length; i++) {
+        let maxScore = score[i][k];
+        let maxStud = i;
+
+        for (let j = i+1; j < score.length; j++) {
+            if (maxScore < score[j][k]) {
+                maxScore = score[j][k];
+                maxStud = j;
+            }
+        }
+
+        //swapping the first with max
+        [score[i], score[maxStud]] = [score[maxStud], score[i]];
+    }
+
+    return score;
 };
