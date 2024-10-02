59f42008f0f53c23d7d9ee923867f77a22b0eebc
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-10-02 22:48:04 +0530

    Time: 90 ms (51.13%), Space: 65 MB (15.79%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let set = new Set(nums);
+    let res = [];
+
+    for (let i=1; i<=nums.length; i++) {
+        if (!set.has(i)) res.push(i);
+    }
+
+    return res;
+};
