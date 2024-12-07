82ffd2db0bc8ef115dceba8b753ac59ff1acbdea
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-07 23:08:17 +0530

    Time: 5 ms (72.01%), Space: 51.9 MB (24.35%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,6 +4,7 @@
  * @return {character}
  */
 var findTheDifference = function (s, t) {
+    //solution using Hashmap
     let track = {};
     for (const char of s) track[char] = 1 + (track[char] || 0)
     for (const char of t) {
@@ -11,4 +12,13 @@ var findTheDifference = function (s, t) {
         track[char]--;
         if (track[char] == -1) return char;
     }
+
+    //Solution using 2 pointer and sort
+    // const sortedS = s.split('').sort((a,b) => a.localeCompare(b)).join('');
+    // const sortedT = t.split('').sort((a,b) => a.localeCompare(b)).join('');
+
+    // let i;
+    // for (i=0; i<s.length; i++) if (sortedS[i] != sortedT[i]) return sortedT[i];
+
+    // return sortedT[i];
 };
