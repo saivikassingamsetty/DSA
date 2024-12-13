2d0bb11de003b73945baa90e55b5b10329aef872
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-13 11:12:23 +0530

    Time: 17 ms (96.42%), Space: 64.8 MB (9.48%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
@@ -3,15 +3,17 @@
  * @return {string}
  */
 var removeStars = function(s) {
-    let res = [];
+    let res = '';
+    let stars = 0;
 
-    for (let ch of s) {
-        if (ch == '*') {
-            if (res.length) res.pop();
-        } else {
-            res.push(ch);
-        }
+    for (let ch of s.split('').reverse()) {
+        //when star found
+        if (ch == '*') stars++;
+        //when non-star found but we have star count
+        else if (stars) stars--;
+        //when non-star found but we dont have star count
+        else res = ch + res;
     }
 
-    return res.join('');
+    return res;
 };
