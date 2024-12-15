bb7cf6bfcc4b6617d003c10bba3a46dbca3c4b5f
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-15 14:12:14 +0530

    Time: 2 ms (77.86%), Space: 49.6 MB (74.64%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.js
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.js
@@ -3,37 +3,20 @@
  * @return {number}
  */
 var countBattleships = function (board) {
-    //number of islands
+    //can be solved as a variant to number of islands
+
     let res = 0;
-    let visited = new Set();
-    let queue = [];
     let m = board.length;
     let n = board[0].length;
 
-    const bfs = (x, y) => {
-        queue = [];
-        queue.push([x, y]);
-
-        while (queue.length) {
-            let [i, j] = queue.shift();
-            visited.add(`${i} ${j}`);
-
-            for (let [di, dj] of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
-                if (i + di >= 0 && i + di < m && j + dj >= 0 && j + dj < n) {
-                    if (!visited.has(`${i + di} ${j + dj}`) && board[i + di][j + dj] == 'X') {
-                        queue.push([i + di, j + dj]);
-                        visited.add(`${i + di} ${j + dj}`);
-                    }
-                }
-            }
-        }
-    }
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (!visited.has(`${i} ${j}`) && board[i][j] == 'X') {
-                bfs(i, j);
+            if (board[i][j] == 'X') {
                 res++;
+                //if surrounded by X
+                if ((i > 0 && board[i-1][j] != '.') || (j > 0 && board[i][j-1] != '.')) {
+                    res--;
+                }
             }
         }
     }
