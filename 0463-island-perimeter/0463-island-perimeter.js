0daaf848b4617a7577d7ccbc0e8ea58489fdd4fa
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-15 15:39:07 +0530

    Time: 8 ms (63.91%), Space: 58 MB (66.44%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.js
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -2,31 +2,48 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var islandPerimeter = function(grid) {
-    let perimeter = 0;
+var islandPerimeter = function (grid) {
+    // let perimeter = 0;
+    // let m = grid.length;
+    // let n = grid[0].length;
+    // let visited = new Set();
+
+    // const dfs = (i, j) => {
+    //     if (j >= n || i >= m || i < 0 || j < 0 || grid[i][j] === 0) {
+    //         perimeter++;
+    //         return;
+    //     }
+
+    //     if (visited.has(`${i} ${j}`)) return;
+
+    //     visited.add(`${i} ${j}`);
+    //     dfs(i+1, j);
+    //     dfs(i-1, j);
+    //     dfs(i, j+1);
+    //     dfs(i, j-1);
+    // }
+
+    // for (let i = 0; i < m; i++) {
+    //     for (let j = 0; j < n; j++) {
+    //         if (!visited.has(`${i} ${j}`) && grid[i][j] == 1) {
+    //             dfs(i, j);
+    //         }
+    //     }
+    // }
+
+    // return perimeter;
+
     let m = grid.length;
     let n = grid[0].length;
-    let visited = new Set();
-
-    const dfs = (i, j) => {
-        if (j >= n || i >= m || i < 0 || j < 0 || grid[i][j] === 0) {
-            perimeter++;
-            return;
-        }
-        
-        if (visited.has(`${i} ${j}`)) return;
-
-        visited.add(`${i} ${j}`);
-        dfs(i+1, j);
-        dfs(i-1, j);
-        dfs(i, j+1);
-        dfs(i, j-1);
-    }
+    let perimeter = 0;
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (!visited.has(`${i} ${j}`) && grid[i][j] == 1) {
-                dfs(i, j);
+            if (grid[i][j] == 1) {
+                if (i == 0 || grid[i - 1][j] == 0) perimeter++;
+                if (i == m - 1 || grid[i + 1][j] == 0) perimeter++;
+                if (j == 0 || grid[i][j - 1] == 0) perimeter++;
+                if (j == n - 1 || grid[i][j + 1] == 0) perimeter++;
             }
         }
     }
