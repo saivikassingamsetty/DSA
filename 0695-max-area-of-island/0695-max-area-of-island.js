5f1c0b61723f590b9dc44ce46024dd1f137dd2b3
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-15 15:26:04 +0530

    Time: 2 ms (96.08%), Space: 51.4 MB (96.39%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -4,30 +4,23 @@
  */
 var maxAreaOfIsland = function(grid) {
     let res = 0;
-    let visited = new Set();
     let m = grid.length;
     let n = grid[0].length;
 
     const dfs = (i,j) => {
-        if (j >= n || i >= m || i < 0 || j < 0 || grid[i][j] === 0) return;
+        if (j >= n || i >= m || i < 0 || j < 0 || grid[i][j] === 0) return 0;
 
-        if (visited.has(`${i} ${j}`)) return;
+        //instead of visited
+        grid[i][j] = 0;
 
-        visited.add(`${i} ${j}`);
-
-        dfs(i+1, j)
-        dfs(i-1, j)
-        dfs(i, j+1)
-        dfs(i, j-1);
+        return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1);
     }
 
     
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (!visited.has(`${i} ${j}`) && grid[i][j] == 1) {
-                let prev = visited.size;
-                dfs(i, j);
-                res = Math.max(res, visited.size - prev);
+            if (grid[i][j] == 1) {
+                res = Math.max(res, dfs(i, j));
             }
         }
     }
