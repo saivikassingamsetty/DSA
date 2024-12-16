5cb692113f043f8a8c248239fc53be6e5b4b16c8
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-16 08:47:37 +0530

    Time: 16 ms (8.98%), Space: 58.1 MB (39.58%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.js
+++ 0113-path-sum-ii/0113-path-sum-ii.js
@@ -11,7 +11,9 @@
  * @param {number} targetSum
  * @return {number[][]}
  */
-var pathSum = function (root, targetSum) {
+
+//recursive + extra memory
+var pathSum1 = function (root, targetSum) {
     let res = [];
 
     const findPathSum = (node, pathSoFar, sumBalanceRemained) => {
@@ -32,3 +34,80 @@ var pathSum = function (root, targetSum) {
 
     return res;
 };
+
+//recursive without extra memory
+var pathSum2 = function (root, targetSum) {
+    const findPathSum = (node, sumBalanceRemained) => {
+        if (!node) return [];
+
+        if (!node.left && !node.right) {
+            if (sumBalanceRemained == node.val) {
+                return [[node.val]];
+            }
+            return [];
+        }
+
+        let left = findPathSum(node.left, sumBalanceRemained - node.val);
+        let right = findPathSum(node.right, sumBalanceRemained - node.val);
+        let vaildPaths = [...left, ...right];
+
+        return vaildPaths.reduce((res, validPath) => {
+            res.push([node.val, ...validPath]);
+            return res;
+        }, []);
+    }
+
+    return findPathSum(root, targetSum);
+};
+
+// BFS + Queue
+var pathSum = function (root, targetSum) {
+    if (!root) return [];
+
+    let queue = [];
+    let res = [];
+    queue.push([root, targetSum, []]);
+
+    while (queue.length) {
+        let [node, sumRemained, pathSoFar] = queue.shift();
+
+        if (!node.left && !node.right && sumRemained == node.val) {
+            res.push([...pathSoFar, node.val]);
+        }
+
+        if (node.left)
+            queue.push([node.left, sumRemained - node.val, [...pathSoFar, node.val]]);
+
+        if (node.right)
+            queue.push([node.right, sumRemained - node.val, [...pathSoFar, node.val]]);
+
+    }
+
+    return res;
+}
+
+// DFS + Stack
+var pathSum4 = function (root, targetSum) {
+    if (!root) return [];
+
+    let stack = [];
+    let res = [];
+    stack.push([root, targetSum, []]);
+
+    while (stack.length) {
+        let [node, sumRemained, pathSoFar] = stack.pop();
+
+        if (!node.left && !node.right && sumRemained == node.val) {
+            res.push([...pathSoFar, node.val]);
+        }
+
+        if (node.left)
+            stack.push([node.left, sumRemained - node.val, [...pathSoFar, node.val]]);
+
+        if (node.right)
+            stack.push([node.right, sumRemained - node.val, [...pathSoFar, node.val]]);
+
+    }
+
+    return res;
+}
