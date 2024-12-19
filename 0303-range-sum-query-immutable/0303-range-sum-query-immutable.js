85b6a28d250b665d4243248167963e64890c861f
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-19 23:14:31 +0530

    Time: 65 ms (44.07%), Space: 60.2 MB (19.57%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+    this.array = nums;
+    this.prefixArray = nums.reduce((a, i) => {
+        return [...a, (a[a.length - 1] || 0) + i]
+    }, []);
+    console.log(this.prefixArray)
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+    return this.prefixArray[right] - (this.prefixArray[left] - this.array[left]);
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
