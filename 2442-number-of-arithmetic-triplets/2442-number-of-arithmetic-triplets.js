dfcdf3611c3e3934b5baa0827c9420e77b825451
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-20 22:31:44 +0530

    Time: 32 ms (29.14%), Space: 49.5 MB (47.43%) - LeetHub

---
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function (nums, diff) {
+    let res = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if ((nums[j] - nums[i]) == diff && (nums[k] - nums[j]) == diff) res++;
+            }
+        }
+    }
+
+    return res;
+};
