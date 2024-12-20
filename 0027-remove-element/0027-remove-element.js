a51a2d844875212021552d7601861962fec7e3f0
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-21 00:01:02 +0530

    Time: 0 ms (100%), Space: 49.1 MB (64.48%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let i = 0;
+    for (let num of nums) {
+        if (num != val) nums[i++] = num;
+    }
+
+    return i;
+};
