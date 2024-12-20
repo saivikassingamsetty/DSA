73beb721e2dff280ff89560bcbbbbc917faca75f
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-20 22:39:20 +0530

    Time: 2 ms (55.43%), Space: 50.1 MB (29.71%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.js
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.js
@@ -6,11 +6,25 @@
 var arithmeticTriplets = function (nums, diff) {
     let res = 0;
 
+    let binarySearch = (target, start, end) => {
+        if (start < 0 || end >= nums.length) return false;
+
+        let low = start;
+        let high = end;
+
+        while (low <= high) {
+            let mid = low + Math.floor((high - low) / 2);
+            if (nums[mid] == target) return true;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                if ((nums[j] - nums[i]) == diff && (nums[k] - nums[j]) == diff) res++;
-            }
+        if (binarySearch(nums[i] - diff, 0, i - 1) && binarySearch(nums[i] + diff, i + 1, nums.length - 1)) {
+            res++;
         }
     }
 
