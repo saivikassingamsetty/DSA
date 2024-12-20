e1590dec1355b66038c45a25fa3f49e2f3a4d1a7
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-21 00:58:16 +0530

    Time: 3 ms (9.46%), Space: 55.2 MB (93.96%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,7 +3,10 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-    for (let i = 0; i < Math.floor(s.length / 2); i++) {
-        [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
+    let i=0;
+    let j=s.length-1;
+
+    while(i<j) {
+        [s[i++], s[j--]] = [s[j], s[i]];
     }
 };
