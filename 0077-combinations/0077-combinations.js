9f0f52fb821331944c6a1e911dcfb1a85f64f63e
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-23 23:10:23 +0530

    Time: 45 ms (93.93%), Space: 121.5 MB (55.87%) - LeetHub

---
+++ 0077-combinations/0077-combinations.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+    let res = [];
+
+    const backtrack = (start, path) => {
+        if (path.length == k) {
+            res.push([...path]);
+            return;
+        }
+
+        for (let i = start; i <= n; i++) {
+            path.push(i);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(1, []);
+
+    return res;
+
+};
