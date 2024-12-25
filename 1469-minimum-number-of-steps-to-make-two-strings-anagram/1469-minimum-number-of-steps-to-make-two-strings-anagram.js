9a3a2fdc380d2dd98abad39713713fd5eb7e8a9e
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-25 23:19:38 +0530

    Time: 25 ms (64.83%), Space: 54.9 MB (86.9%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -3,16 +3,10 @@
  * @param {string} t
  * @return {number}
  */
-var minSteps = function(s, t) {
+var minSteps = function (s, t) {
     let freqArray = new Array(26).fill(0);
     for (let ch of s) freqArray[ch.charCodeAt(0) - 97]++;
     for (let ch of t) freqArray[ch.charCodeAt(0) - 97]--;
 
-    let posCount = 0;
-
-    for (let value of freqArray) {
-        if (value > 0) posCount += value;
-    }
-
-    return posCount;
+    return freqArray.reduce((a, i) => a + Math.max(i, 0), 0);
 };
