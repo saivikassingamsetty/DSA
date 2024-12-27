f96c31b1242ab79e7278cbe2d358aed1254e6b93
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-27 17:57:55 +0530

    Time: 63 ms (92.59%), Space: 73.7 MB (100%) - LeetHub

--- 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.js
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.js
@@ -6,7 +6,13 @@ var countDistinctIntegers = function(nums) {
     let res = new Set(nums);
 
     for (let num of nums) {
-        res.add(Number(num.toString().split('').reverse().join('')));
+        // res.add(Number(num.toString().split('').reverse().join('')));
+        let reversed = 0;
+        while(num) {
+            reversed = reversed*10 + num%10;
+            num = Math.floor(num/10);
+        }
+        res.add(reversed);
     }
 
     return res.size;
