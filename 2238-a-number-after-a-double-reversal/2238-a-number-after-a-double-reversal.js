53114b065f2ed04ebfcc147636443f1593bfb59d
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2024-12-27 20:45:13 +0530

    Time: 0 ms (100%), Space: 49 MB (27.94%) - LeetHub

--- 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.js
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.js
@@ -3,17 +3,18 @@
  * @return {boolean}
  */
 var isSameAfterReversals = function (num) {
-    let reversed = 0;
-    let copy = num;
-    while (num) {
-        reversed = reversed * 10 + num % 10;
-        num = Math.floor(num / 10);
-    }
-    let reversed2 = 0;
-    while (reversed) {
-        reversed2 = reversed2 * 10 + reversed % 10;
-        reversed = Math.floor(reversed / 10);
-    }
+    // let reversed = 0;
+    // let copy = num;
+    // while (num) {
+    //     reversed = reversed * 10 + num % 10;
+    //     num = Math.floor(num / 10);
+    // }
+    // let reversed2 = 0;
+    // while (reversed) {
+    //     reversed2 = reversed2 * 10 + reversed % 10;
+    //     reversed = Math.floor(reversed / 10);
+    // }
 
-    return reversed2 == copy;
+    // return reversed2 == copy;
+    return num == 0 || num%10 != 0;
 };
