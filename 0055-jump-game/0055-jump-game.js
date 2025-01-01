717825aaa01622a9c1525abbe5ca67569a5cdcfe
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-01 22:44:46 +0530

    Time: 1 ms (88.01%), Space: 53.2 MB (76.68%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,19 +3,28 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-    let n = nums.length;
-    let dp = new Array(n).fill(false);
+    // let n = nums.length;
+    // let dp = new Array(n).fill(false);
 
-    dp[n - 1] = true;
+    // dp[n - 1] = true;
 
-    for (let i = n - 2; i >= 0; i--) {
-        for (let j = i + 1; j <= i + nums[i]; j++) {
-            if (dp[j]) {
-                dp[i] = true;
-                break;
-            }
+    // for (let i = n - 2; i >= 0; i--) {
+    //     for (let j = i + 1; j <= i + nums[i]; j++) {
+    //         if (dp[j]) {
+    //             dp[i] = true;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // return dp[0];
+
+    let goal = nums.length - 1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= goal) {
+            goal = i;
         }
     }
 
-    return dp[0];
+    return goal == 0;
 };
