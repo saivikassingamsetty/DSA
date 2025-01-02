64552b449a600f243364ee490d432024bf627969
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-02 23:05:29 +0530

    Time: 2 ms (97.03%), Space: 63 MB (68.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,17 +3,31 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    let leftPrefixProduct = new Array(nums.length).fill(1);
-    let rightPrefixProduct = new Array(nums.length).fill(1);
+    // let leftPrefixProduct = new Array(nums.length).fill(1);
+    // let rightPrefixProduct = new Array(nums.length).fill(1);
 
-    for (let i = 0; i < nums.length; i++) {
-        leftPrefixProduct[i] = (leftPrefixProduct[i - 1] ?? 1) * nums[i];
-        rightPrefixProduct[nums.length - 1 - i] = (rightPrefixProduct[nums.length - i] ?? 1) * nums[nums.length - 1 - i];
+    // for (let i = 0; i < nums.length; i++) {
+    //     leftPrefixProduct[i] = (leftPrefixProduct[i - 1] ?? 1) * nums[i];
+    //     rightPrefixProduct[nums.length - 1 - i] = (rightPrefixProduct[nums.length - i] ?? 1) * nums[nums.length - 1 - i];
+    // }
+
+    // let res = [];
+    // for (let i = 0; i < nums.length; i++) {
+    //     res.push((leftPrefixProduct[i - 1] ?? 1) * (rightPrefixProduct[i + 1] ?? 1));
+    // }
+
+    // return res;
+    let res = new Array(nums.length).fill(1);
+    let prevProduct = 1;
+    for (let i=0; i<nums.length; i++) {
+        res[i] *= prevProduct;
+        prevProduct *= nums[i]; 
     }
 
-    let res = [];
-    for (let i = 0; i < nums.length; i++) {
-        res.push((leftPrefixProduct[i - 1] ?? 1) * (rightPrefixProduct[i + 1] ?? 1));
+    prevProduct = 1;
+    for (let i=nums.length-1; i>=0; i--) {
+        res[i] *= prevProduct;
+        prevProduct *= nums[i];
     }
 
     return res;
