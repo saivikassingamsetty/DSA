df02b661fdeda01ea6255977917866eeffb9c1eb
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-05 20:15:31 +0530

    Time: 5 ms (21.58%), Space: 51.8 MB (13.4%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,5 +3,24 @@
  * @return {string}
  */
 var reverseWords = function (s) {
-    return s.trim().split(' ').filter(i => i.length > 0).reverse().join(' ');
+    // return s.trim().split(' ').filter(i => i.length > 0).reverse().join(' ');
+
+    let start = 0;
+    let end = 1;
+    let res = '';
+
+    for (end=1; end < s.length; end++) {
+        if (s[end] == ' ') {
+            if (s[end-1] == ' ') continue;
+            res = ' ' + s.slice(start, end) + res;
+            start = end;
+        } else {
+            if (s[end-1] == ' ') {
+                start = end;
+            }
+        }
+    }
+
+    res = s.slice(start, end) + res;
+    return res.trim();
 };
