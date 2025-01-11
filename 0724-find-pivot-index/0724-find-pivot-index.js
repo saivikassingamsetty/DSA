c0f92eab032cbbe6e64d7210b07b30f3a5a8965a
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-11 23:45:40 +0530

    Time: 1 ms (86.73%), Space: 51.8 MB (78.83%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,18 +3,28 @@
  * @return {number}
  */
 var pivotIndex = function (nums) {
-    let leftSum = nums.reduce((acc, i, index) => {
-        acc.push((acc[index - 1] || 0) + i);
-        return acc;
-    }, []);
+    // let leftSum = nums.reduce((acc, i, index) => {
+    //     acc.push((acc[index - 1] || 0) + i);
+    //     return acc;
+    // }, []);
 
-    let rightSum = nums.reverse().reduce((acc, i, index) => {
-        acc.push((acc[index - 1] || 0) + i);
-        return acc;
-    }, []).reverse();
+    // let rightSum = nums.reverse().reduce((acc, i, index) => {
+    //     acc.push((acc[index - 1] || 0) + i);
+    //     return acc;
+    // }, []).reverse();
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     if ((leftSum[i-1] || 0) == (rightSum[i+1] || 0)) return i;
+    // }
+
+    // return -1;
+
+    let totalSum = nums.reduce((a, i) => a + i, 0);
+    let leftSum = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        if ((leftSum[i-1] || 0) == (rightSum[i+1] || 0)) return i;
+        if (leftSum == totalSum - leftSum - nums[i]) return i;
+        leftSum += nums[i];
     }
 
     return -1;
