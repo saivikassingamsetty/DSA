3844117540b0b992a7d41dc4b2564001132941c6
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-16 20:21:12 +0530

    Time: 7 ms (72.84%), Space: 57.6 MB (88.12%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,9 +3,19 @@
  * @return {number}
  */
 var findDuplicate = function (nums) {
-    let a = new Set();
-    for (let num of nums) {
-        if (a.has(num)) return num;
-        a.add(num);
+    let slow = 0;
+    let fast = 0;
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast)
+
+    slow = 0;
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
     }
+
+    return slow;
 };
