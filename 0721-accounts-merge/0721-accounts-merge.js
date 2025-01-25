24f42992d051f85b6a6ec7ce838b069ce71d2b7b
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-26 02:16:03 +0530

    Time: 122 ms (10.71%), Space: 64.3 MB (30.46%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -0,0 +1,65 @@
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function (accounts) {
+    let parents = new Map();
+    let names = new Map();
+
+    function find(node) {
+        if (!parents.get(node)) {
+            parents.set(node, node);
+        }
+
+        if (parents.get(node) !== node) {
+            parents.set(node, find(parents.get(node)));
+        }
+
+        return parents.get(node);
+    }
+
+    function union(u, v, name) {
+        const rootU = find(u);
+        const rootV = find(v);
+
+        if (rootU != rootV) {
+            parents.set(rootU, rootV);
+        }
+    }
+
+    for (let account of accounts) {
+        let name = account[0];
+        let firstEmail = account[1];
+
+        for (let j = 1; j < account.length; j++) {
+            let email = account[j];
+            union(firstEmail, email);
+            names.set(email, name);
+        }
+    }
+
+    let emailsGroup = new Map();
+    for (let email of parents.keys()) {
+        const root = find(email);
+        if (!emailsGroup.has(root)) {
+            emailsGroup.set(root, []);
+        }
+
+        emailsGroup.set(root, [...emailsGroup.get(root), email]);
+    }
+
+    console.log(emailsGroup)
+
+    let res = [];
+    for (let [emailRoot, emails] of emailsGroup) {
+        let name = names.get(emailRoot);
+        let emailsCopy = [...new Set(emails)].sort((a, b) => {
+            if (a < b) return -1
+            else return 1
+        });
+
+        res.push([name, ...emailsCopy]);
+    }
+
+    return res;
+};
