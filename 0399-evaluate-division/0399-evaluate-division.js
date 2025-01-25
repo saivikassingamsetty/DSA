213c00da5f023ba1a3618caaf3148de3477dd4d3
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-01-26 00:26:55 +0530

    Time: 0 ms (100%), Space: 49.1 MB (66.12%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.js
+++ 0399-evaluate-division/0399-evaluate-division.js
@@ -1,50 +1,33 @@
 class DisjointSet {
     constructor() {
         this.parent = new Map();
-        this.size = new Map();
         this.vals = new Map();
     }
 
     findParent(node) {
-        if (!this.parent.has(node)) this.parent.set(node, node);
+        if (!this.parent.has(node)) {
+            this.parent.set(node, node);
+            this.vals.set(node, 1);
+        }
 
-        if (this.parent.get(node) === node) return node;
-        let par = this.findParent(this.parent.get(node));
+        if (this.parent.get(node) !== node) {
+            const originalParent = this.parent.get(node);
+            const parent = this.findParent(originalParent);
+            this.parent.set(node, parent);
+            this.vals.set(node, this.vals.get(node) * this.vals.get(originalParent));
+        }
 
-        this.vals.set(node, this.vals.get(node) * this.vals.get(this.parent.get(node)));
-        this.parent.set(node, par);
-
-        return par;
+        return this.parent.get(node);
     }
 
-    unionBySize(u, v, value) {
-        if (!this.vals.has(u)) this.vals.set(u, 1);
-        if (!this.vals.has(v)) this.vals.set(v, 1);
+    union(u, v, value) {
+        let parentU = this.findParent(u);
+        let parentV = this.findParent(v);
 
-        const ultimateParentU = this.findParent(u);
-        const ultimateParentV = this.findParent(v);
-
-        if (ultimateParentU === ultimateParentV) return;
-
-        const ratio = (value * this.vals.get(v)) / this.vals.get(u);
-
-        if (!this.size.has(ultimateParentU)) this.size.set(ultimateParentU, 1);
-        if (!this.size.has(ultimateParentV)) this.size.set(ultimateParentV, 1);
-
-        if (this.size.get(ultimateParentU) < this.size.get(ultimateParentV)) {
-            this.parent.set(ultimateParentU, ultimateParentV);
-            this.size.set(
-                ultimateParentV,
-                this.size.get(ultimateParentU) + this.size.get(ultimateParentV)
-            );
-            this.vals.set(ultimateParentU, ratio);
-        } else {
-            this.parent.set(ultimateParentV, ultimateParentU);
-            this.size.set(
-                ultimateParentU,
-                this.size.get(ultimateParentU) + this.size.get(ultimateParentV)
-            );
-            this.vals.set(ultimateParentV, 1 / ratio);
+        if (parentU !== parentV) {
+            const ratio = this.vals.get(v) * value / this.vals.get(u);
+            this.parent.set(parentU, parentV);
+            this.vals.set(parentU, ratio);
         }
     }
 }
@@ -57,18 +40,12 @@ class DisjointSet {
  */
 var calcEquation = function (equations, values, queries) {
     let ds = new DisjointSet();
-    for (let i = 0; i < equations.length; i++) {
-        ds.unionBySize(equations[i][0], equations[i][1], values[i]);
-    }
+    equations.forEach(([u, v], i) => ds.union(u, v, values[i]));
 
-    let res = [];
-    for (let [u, v] of queries) {
-        if (!ds.vals.has(u) || !ds.vals.has(v) || ds.findParent(u) != ds.findParent(v)) {
-            res.push(-1)
-        } else {
-            res.push(ds.vals.get(u) / ds.vals.get(v));
+    return queries.map(([u, v]) => {
+        if (!ds.vals.has(u) || !ds.vals.has(v) || ds.findParent(u) !== ds.findParent(v)) {
+            return -1;
         }
-    }
-
-    return res;
-};
+        return ds.vals.get(u) / ds.vals.get(v);
+    });
+}
