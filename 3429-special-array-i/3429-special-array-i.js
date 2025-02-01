0ce7ff3f70b73350e470e6fc103f748aeecb8402
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-02-01 23:18:12 +0530

    Time: 0 ms (100%), Space: 50.7 MB (99.53%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.js
+++ 3429-special-array-i/3429-special-array-i.js
@@ -3,11 +3,8 @@
  * @return {boolean}
  */
 var isArraySpecial = function (nums) {
-    let parity = nums[0] % 2;
-
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] % 2 == parity) return false;
-        parity = 1 - parity;
+        if (nums[i] % 2 == nums[i - 1] % 2) return false;
     }
 
     return true;
