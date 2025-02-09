11e54c665ba915708fc456dceee6f7a618db49b9
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-02-09 12:21:56 +0530

    Time: 90 ms (57.89%), Space: 65.6 MB (57.89%) - LeetHub

--- 0639-decode-ways-ii/0639-decode-ways-ii.js
+++ 0639-decode-ways-ii/0639-decode-ways-ii.js
@@ -3,39 +3,38 @@
  * @return {number}
  */
 var numDecodings = function (s) {
-    let memo = new Map();
     let MOD = 10 ** 9 + 7;
+    let n = s.length
+    let dp = new Array(n + 1).fill(0);
 
-    const findWays = (i) => {
-        if (i < 0) return 1;
+    dp[0] = 1;
+    dp[1] = (s[0] == '*' ? 9 : (s[0] == 0 ? 0 : 1));
 
-        if (memo.has(i)) return memo.get(i);
-
+    for (let i = 2; i <= n; i++) {
         let ways = 0;
 
-        if (s[i] == '*') {
-            ways = (9 * findWays(i - 1)) % MOD;
+        if (s[i - 1] == '*') {
+            ways = (9 * dp[i - 1]) % MOD;
             //1*
-            if (i > 0 && s[i - 1] == 1) ways = (ways + 9 * findWays(i - 2)) % MOD;
+            if (i > 0 && s[i - 2] == 1) ways = (ways + 9 * dp[i - 2]) % MOD;
             //2*
-            if (i > 0 && s[i - 1] == 2) ways = (ways + 6 * findWays(i - 2)) % MOD;
+            if (i > 0 && s[i - 2] == 2) ways = (ways + 6 * dp[i - 2]) % MOD;
             //**
-            if (i > 0 && s[i - 1] == '*') ways = (ways + 15 * findWays(i - 2)) % MOD;
+            if (i > 0 && s[i - 2] == '*') ways = (ways + 15 * dp[i - 2]) % MOD;
         } else {
-            ways = s[i] == 0 ? 0 : findWays(i - 1);
+            ways = s[i - 1] == 0 ? 0 : dp[i - 1];
             //1#
-            if (i > 0 && s[i - 1] == 1) ways = (ways + findWays(i - 2)) % MOD
+            if (i > 0 && s[i - 2] == 1) ways = (ways + dp[i - 2]) % MOD
             //2#
-            if (i > 0 && s[i - 1] == 2 && s[i] <= 6) ways = (ways + findWays(i - 2)) % MOD
+            if (i > 0 && s[i - 2] == 2 && s[i - 1] <= 6) ways = (ways + dp[i - 2]) % MOD
             //*# - 1
-            if (i > 0 && s[i - 1] == '*') ways = (ways + findWays(i - 2)) % MOD;
+            if (i > 0 && s[i - 2] == '*') ways = (ways + dp[i - 2]) % MOD;
             //*# - 2
-            if (i > 0 && s[i - 1] == '*' && s[i] <= 6) ways = (ways + findWays(i - 2)) % MOD;
+            if (i > 0 && s[i - 2] == '*' && s[i - 1] <= 6) ways = (ways + dp[i - 2]) % MOD;
         }
 
-        memo.set(i, ways);
-        return ways;
+        dp[i] = ways;
     }
 
-    return findWays(s.length - 1);
+    return dp[n];
 }
