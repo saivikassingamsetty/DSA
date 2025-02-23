907d6cfb513a6032afeb5f8df243ade2f79c5915
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-02-23 18:45:17 +0530

    Time: 76 ms (77.5%), Space: 65.2 MB (6.25%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.js
@@ -0,0 +1,37 @@
+var serialize = function (root) {
+    if (!root) return '';
+
+    const postOrder = [];
+
+    const traverse = (node) => {
+        if (!node) {
+            postOrder.push("null");
+            return;
+        }
+        traverse(node.left);
+        traverse(node.right);
+        postOrder.push(node.val.toString());
+    };
+
+    traverse(root);
+
+    return postOrder.join(' ');
+};
+
+var deserialize = function (data) {
+    console.log(data)
+    if (!data.trim()) return null;
+    data = data.split(' ');
+
+    function buildTree() {
+        const val = data.pop();
+        if (val === "null") return null;
+
+        const node = new TreeNode(Number(val));
+        node.right = buildTree();
+        node.left = buildTree();
+        return node;
+    }
+
+    return buildTree();
+};
