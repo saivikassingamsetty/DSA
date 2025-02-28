c049b482e708ea47b5258daf33acdbcc5c735133
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-02-28 19:34:44 +0530

    Time: 44 ms (84.73%), Space: 86.6 MB (14.09%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+    let m = text1.length;
+    let n = text2.length;
+    //lcs of stroing 1 length i, string 2 length j
+    let dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+};
