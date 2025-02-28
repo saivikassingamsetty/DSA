be69cf797fbe162059aad32bc868e85d1eb66fe7
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-02-28 21:00:31 +0530

    Time: 16 ms (79.44%), Space: 61.6 MB (12.15%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.js
@@ -0,0 +1,27 @@
+var LCS = function (text1, text2) {
+    let m = text1.length;
+    let n = text2.length;
+    //lcs of stroing 1 length i, string 2 length j
+    let dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+};
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+    return word1.length + word2.length - 2 * LCS(word1, word2);
+};
