b4b4c00c48c74f95a9acdb155ae198e9282d9f81
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-03-03 10:17:17 +0530

    Time: 51 ms (17.13%), Space: 83.7 MB (55.25%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.js
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.js
@@ -4,5 +4,21 @@
  * @return {number[]}
  */
 var pivotArray = function (nums, pivot) {
-    return [...nums.filter(i => i < pivot), ...nums.filter(i => i == pivot), ...nums.filter(i => i > pivot)];
+    // return [...nums.filter(i => i < pivot), ...nums.filter(i => i == pivot), ...nums.filter(i => i > pivot)];
+
+    const res = [];
+    let left = 0;
+    let right = nums.length - 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < pivot) res[left++] = nums[i];
+        if (nums[nums.length - i - 1] > pivot) res[right--] = nums[nums.length - i - 1];
+    }
+
+    while (left <= right) {
+        res[left++] = pivot;
+        res[right--] = pivot;
+    }
+
+    return res;
 };
