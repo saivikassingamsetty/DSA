3540931b2c564a106bc1463d2a0736c597b8ba11
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-03-30 23:20:56 +0530

    Time: 5 ms (69.43%), Space: 54.4 MB (60.46%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.js
+++ 0768-partition-labels/0768-partition-labels.js
@@ -27,14 +27,35 @@ var merge = function (intervals) {
 };
 
 var partitionLabels = function (s) {
-    let indeces = new Array(26).fill().map(() => [-1, -1]);
+    // let indeces = new Array(26).fill().map(() => [-1, -1]);
 
+    // for (let i = 0; i < s.length; i++) {
+    //     if (indeces[s[i].charCodeAt(0) - 97][0] == -1) indeces[s[i].charCodeAt(0) - 97][0] = i;
+    //     indeces[s[i].charCodeAt(0) - 97][1] = i;
+    // }
+
+    // indeces = indeces.filter(([a, b]) => a != -1 && b != -1);
+
+    // return merge(indeces).map(([a,b]) => b-a+1);
+
+    let last = new Map();
     for (let i = 0; i < s.length; i++) {
-        if (indeces[s[i].charCodeAt(0) - 97][0] == -1) indeces[s[i].charCodeAt(0) - 97][0] = i;
-        indeces[s[i].charCodeAt(0) - 97][1] = i;
+        last.set(s[i], i);
     }
 
-    indeces = indeces.filter(([a, b]) => a != -1 && b != -1);
+    let res = [];
+    let anchor = 0;
+    let j = 0;
+    for (let i = 0; i < s.length; i++) {
+        //equal to extending the interval
+        j = Math.max(j, last.get(s[i]));
 
-    return merge(indeces).map(([a,b]) => b-a+1);
+        //when we reach the end of one phase, we can break it
+        if (i == j) {
+            res.push(i - anchor + 1);
+            anchor = i + 1;
+        }
+    }
+
+    return res;
 };
