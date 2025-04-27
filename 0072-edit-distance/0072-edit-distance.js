16178e82f5df3650d048b98997e923b450fa73ae
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-04-27 18:59:02 +0530

    Time: 8 ms (77.82%), Space: 60 MB (55.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -3,37 +3,32 @@
  * @param {string} word2
  * @return {number}
  */
-var minDistance = function(word1, word2) {
-    // Use memoization to avoid redundant calculations
-    const memo = {};
-    
-    const solve = (i, j) => {
-        // Create a unique key for memoization
-        const key = `${i},${j}`;
-        if (key in memo) return memo[key];
-        
-        // If we've processed all characters in word1
-        if (i === word1.length) {
-            return word2.length - j; // Insert all remaining characters from word2
+var minDistance = function (word1, word2) {
+    let dp = Array.from({ length: word1.length + 1 }, () => new Array(word2.length + 1).fill(0));
+    let m = word1.length;
+    let n = word2.length;
+
+    //delete all items in word1
+    for (let i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+
+    //insert all items in word1
+    for (let j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (word1[i - 1] == word2[j - 1]) {
+                // no extra steps
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                //3 ways to correct
+                dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+            }
         }
-        
-        // If we've processed all characters in word2
-        if (j === word2.length) {
-            return word1.length - i; // Delete all remaining characters from word1
-        }
-        
-        // If characters match, no operation needed
-        if (word1[i] === word2[j]) {
-            return memo[key] = solve(i + 1, j + 1);
-        } else {
-            // Try all three operations and take the minimum
-            return memo[key] = 1 + Math.min(
-                solve(i, j + 1),     // Insert
-                solve(i + 1, j),     // Delete
-                solve(i + 1, j + 1)  // Replace
-            );
-        }
-    };
-    
-    return solve(0, 0);
+    }
+
+    return dp[m][n];
 };
