45b15ff9b483ae39d79946c3c87f7619d5828490
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-04-28 23:15:19 +0530

    Time: 3 ms (64.86%), Space: 56.1 MB (54.67%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
@@ -3,33 +3,21 @@
  * @return {number}
  */
 var longestValidParentheses = function (s) {
-    let stack = [];
-    let res = new Array(s.length).fill(0);
+    let stack = [-1];
+    let max = 0;
 
     for (let i = 0; i < s.length; i++) {
         if (s[i] == '(') {
             stack.push(i);
         } else {
-            if (stack.length) {
-                let j = stack.pop();
-                res[j] = 1;
-                res[i] = 1;
+            stack.pop();
+            if (!stack.length) {
+                stack.push(i) // new base
+            } else {
+                max = Math.max(max, i - stack[stack.length - 1]);
             }
         }
     }
 
-    //longest consecutive 1 in 01 array
-    let max = 0;
-    let curr = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        if (res[i]) {
-            curr++;
-        } else {
-            max = Math.max(max, curr);
-            curr = 0;
-        }
-    }
-
-    return Math.max(max, curr);
+    return max;
 };
