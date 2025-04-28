82cf8ba219584fc47cc4c6f62d406a46850dfbc5
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-04-28 20:49:41 +0530

    Time: 12 ms (53.96%), Space: 56.4 MB (60.37%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -6,46 +6,53 @@
 var isMatch = function (s, p) {
     let m = s.length;
     let n = p.length;
+    let memo = {};
 
     const isPossible = (i, j) => {
+        let key = `${i},${j}`;
+        
+        if (key in memo) {
+            return memo[key];
+        }
+
         if (i == m) {
             let k = j;
             while (k < n) {
                 if (k + 1 < n && p[k + 1] == '*') {
                     k += 2;
                 } else {
-                    return false;
+                    return memo[key] = false;
                 }
             }
-            return true;
+            return memo[key] = true;
         }
 
         if (j == n) {
-            return false;
+            return memo[key] = false;
         }
 
         if (p[j] == '.') {
             if (j + 1 < n && p[j + 1] == '*') {
-                return isPossible(i + 1, j) || isPossible(i, j + 2);
+                return memo[key] = isPossible(i + 1, j) || isPossible(i, j + 2);
             } else {
-                return isPossible(i + 1, j + 1);
+                return memo[key] = isPossible(i + 1, j + 1);
             }
         } else if (p[j] != '*') {
             if (j + 1 < n && p[j + 1] == '*') {
                 if (s[i] != p[j]) {
-                    return isPossible(i, j + 2);
+                    return memo[key] = isPossible(i, j + 2);
                 } else {
-                    return isPossible(i + 1, j) || isPossible(i, j + 2);
+                    return memo[key] = isPossible(i + 1, j) || isPossible(i, j + 2);
                 }
             } else {
                 if (s[i] == p[j]) {
-                    return isPossible(i + 1, j + 1);
+                    return memo[key] = isPossible(i + 1, j + 1);
                 } else {
-                    return false;
+                    return memo[key] = false;
                 }
             }
         } else {
-            return false;
+            return memo[key] = false;
         }
     }
 
