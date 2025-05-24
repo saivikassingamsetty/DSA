bfad9d8762eb8e22bd556e0b766f6ac58d6144e9
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-05-24 17:30:38 +0530

    Time: 0 ms (100%), Space: 53.6 MB (39.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,11 +3,23 @@
  * @return {number}
  */
 var rob = function(nums) {
-    let dp = new Array(nums.length).fill(0);
+    // let dp = new Array(nums.length).fill(0);
 
-    for (let i=0; i<nums.length; i++) {
-        dp[i] = Math.max((dp[i-1] || 0), (dp[i-2] || 0) + nums[i]);
+    // for (let i=0; i<nums.length; i++) {
+    //     dp[i] = Math.max((dp[i-1] || 0), (dp[i-2] || 0) + nums[i]);
+    // }
+
+    // return dp[dp.length-1];
+
+    const dfs = (i) => {
+        if (i == 0) {
+            return [nums[i], 0];
+        }
+
+        let [included, excluded] = dfs(i-1);
+        
+        return [nums[i] + excluded, Math.max(included, excluded)];
     }
 
-    return dp[dp.length-1];
+    return Math.max(...dfs(nums.length-1));
 };
