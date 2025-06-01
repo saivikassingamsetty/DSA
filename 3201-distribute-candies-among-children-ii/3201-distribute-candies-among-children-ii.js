9b52614bcd6c21c9a9726a66ff0892a1eb7be00a
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-06-01 08:31:31 +0530

    Time: 111 ms (33.33%), Space: 58.2 MB (16.67%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number} limit
+ * @return {number}
+ */
+var distributeCandies = function (n, limit) {
+    let res = 0;
+    let ways = 0;
+
+    for (let i = 0; i <= limit; i++) {
+        if (i > n) continue
+
+        //Number of ways to distribute (n-i) candies to 2 children
+        // let say remianing are 8 and limit is 8, 0,8, 1,7, (2,6) (3,5), (4,4), (5,3), (6,2), 7,1, 8,0 -9
+        // let say remianing are 8 and limit is 7, 1,7, (2,6) (3,5), (4,4), (5,3), (6,2), 7,1 - 7
+        // let say remianing are 8 and limit is 6, (2,6) (3,5), (4,4), (5,3), (6,2) - 5
+        // let say remianing are 8 and limit is 5, (3,5), (4,4), (5,3) - 3
+        // let say remianing are 8 and limit is 4, (4,4) - 1
+
+        // means remaining+1 - (remaining - limit)*2
+        //sometimes limit can be bigger (edge case, handle that with limiting)
+        ways = (n - i + 1) - (n - i - Math.min(limit, n - i)) * 2;
+        res += (ways > 0 ? ways : 0)
+    }
+
+    return res;
+};
