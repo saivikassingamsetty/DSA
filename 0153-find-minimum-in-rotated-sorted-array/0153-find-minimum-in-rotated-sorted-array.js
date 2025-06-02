130de47876cf42fe0765546db2f5f1631c4c5a14
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-06-03 01:02:06 +0530

    Time: 0 ms (100%), Space: 53.4 MB (67.31%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+    let l = 0;
+    let r = nums.length - 1;
+    let res = 0;
+
+    while (l < r) {
+        let mid = Math.floor((l + r) / 2);
+
+        // left of inflection point
+        if (nums[mid] > nums[r]) {
+            l = mid + 1;
+        } else {
+            //right
+            r = mid;
+        }
+    }
+
+    return nums[l];
+};
