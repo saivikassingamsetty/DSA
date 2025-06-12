06ac70538d84d5454e0c7958eccbea648ef05223
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-06-13 00:08:18 +0530

    Time: 2 ms (75.34%), Space: 57.9 MB (56.11%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -3,18 +3,5 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function (s) {
-    let temp = '';
-    let repeat = 0;
-
-    for (let i = 1; i < s.length; i++) {
-        if (s.length % i === 0) {
-            temp = s.slice(0, i);
-            repeat = Math.floor(s.length / i);
-            if (temp.repeat(repeat) === s) {
-                return true;
-            }
-        }
-    }
-
-    return false;
+    return (s + s).slice(1, -1).includes(s);
 };
