a624b9f37ab2469e11bbcfcce92c0cbfc2a3f9b7
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-06-27 20:08:36 +0530

    Time: 2 ms (86.45%), Space: 56 MB (60.69%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.js
@@ -3,22 +3,19 @@
  * @return {string}
  */
 var removeOuterParentheses = function (s) {
-    let toBeRemoved = 0;
     let balance = 0;
-    let res = [];
+    let res = '';
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == '(') {
-            if (balance == 0) toBeRemoved = i;
+    for (let ch of s) {
+        if (ch == '(') {
+            if (balance) res += ch;
             balance++
         }
         else {
-            balance--;
-            if (balance == 0) {
-                res.push(s.slice(toBeRemoved + 1, i));
-            }
+            balance--
+            if (balance) res += ch;
         }
     }
 
-    return res.join('');
+    return res;
 };
