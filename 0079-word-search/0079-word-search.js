6c913e71d563a5eef12e425e428ed24acb3b35bc
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-07-13 13:48:27 +0530

    Time: 455 ms (32.05%), Space: 60 MB (25.22%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,24 +4,36 @@
  * @return {boolean}
  */
 var exist = function (board, word) {
-    const vis = new Set();
     let m = board.length;
     let n = board[0].length;
 
     const dfs = (i, j, k) => {
         if (k == word.length) return true;
         if (i >= m || i < 0 || j < 0 || j >= n) return false;
-        if (vis.has(`${i},${j}`)) return false;
+        if (board[i][j] == '#') return false;
         if (board[i][j] != word[k]) return false;
 
-        vis.add(`${i},${j}`);
+        board[i][j] = '#';
         for (let [di, dj] of [[-1, 0], [1, 0], [0, 1], [0, -1]]) {
             if (dfs(i + di, j + dj, k + 1)) return true;
         }
-        vis.delete(`${i},${j}`);
+        board[i][j] = word[k];
 
         return false;
     }
+
+    let start = word[0];
+    let end = word[word.length - 1];
+    const freq = { start: 0, end: 0 };
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] == start) freq[start] + 1;
+            else if (board[i][j] == end) freq[end] + 1;
+        }
+    }
+
+    if (freq[start] > freq[end]) return exist(board, word.reverse());
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
