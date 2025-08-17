0d360a72b71b04147dae4eeaf054358910a60907
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-08-17 22:24:58 +0530

    Time: 4 ms (76.49%), Space: 58.9 MB (43.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -7,15 +7,12 @@ var isPalindrome = function (s) {
     let j = s.length - 1;
 
     while (i < j) {
-        if (s[i].match(/^[0-9a-z]$/i) && s[j].match(/^[0-9a-z]$/i)) {
-            if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
-            i++;
-            j--;
-        } else if (!s[i].match(/^[0-9a-z]$/i)) {
-            i++;
-        } else {
-            j--;
-        }
+        while (i < j && !s[i].match(/^[0-9a-z]$/i)) i++;
+        while (j > i && !s[j].match(/^[0-9a-z]$/i)) j--;
+
+        if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+        i++;
+        j--;
     }
 
     return true;
