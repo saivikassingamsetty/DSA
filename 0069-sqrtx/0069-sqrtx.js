5a0a88b8a60a5ea6664998aeda2f5c6561df705d
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-08-29 23:36:37 +0530

    Time: 0 ms (100%), Space: 55.9 MB (56.11%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,6 +2,16 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x));
+var mySqrt = function (x) {
+    let l = 0;
+    let r = x;
+
+    while (l < r) {
+        let mid = Math.floor((l + r + 1) / 2);
+        if (mid * mid == x) return mid
+        else if (mid * mid > x) r = mid - 1
+        else l = mid
+    }
+
+    return l;
 };
