a1dd0c9eb3e04c39ff3e47a2d57fe3433b658a33
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-08-31 01:27:21 +0530

    Time: 4 ms (13.09%), Space: 55.3 MB (33.66%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-    let dp = new Array(cost.length).fill(0);
+    let n = cost.length;
+    let dp = new Array(n).fill(0);
     dp[0] = cost[0];
     dp[1] = cost[1];
 
-    for (let i = 2; i < cost.length; i++) {
-        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+    for (let i = 2; i <= n; i++) {
+        dp[i] = (i == n ? 0 : cost[i]) + Math.min(dp[i - 1], dp[i - 2]);
     }
 
-    return Math.min(dp[cost.length - 2], dp[cost.length - 1]);
+    return dp[n];
 };
