7fbf11b6737f9d82bddc26bb6d9c96dcf8ba0b90
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-09-07 14:06:26 +0530

    Time: 10 ms (100%), Space: 75.4 MB (100%) - LeetHub

--- 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.js
+++ 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.js
@@ -4,15 +4,20 @@
  * @return {number}
  */
 var maxSumDistinctTriplet = function (x, y) {
-    let map = {};
+    let [a, b, c] = [0, 0, 0];
+    let [i1, i2, i3] = [0, 0, 0];
 
     for (let i = 0; i < x.length; i++) {
-        map[x[i]] = Math.max((map[x[i]] ?? -Infinity), y[i]);
+        if (x[i] == i1) a = Math.max(a, y[i])
+        else if (x[i] == i2) b = Math.max(b, y[i])
+        else if (x[i] == i3) c = Math.max(c, y[i])
+        else if (y[i] > a || y[i] > b || y[i] > c) {
+            minI = Math.min(a, b, c);
+            if (minI == a) [a, i1] = [y[i], x[i]]
+            else if (minI == b) [b, i2] = [y[i], x[i]]
+            else[c, i3] = [y[i], x[i]]
+        }
     }
 
-    let values = Object.values(map);
-    if (values.length < 3) return -1;
-
-    values.sort((a, b) => b - a);
-    return values[0] + values[1] + values[2];
+    return ((!a || !b || !c) ? -1 : a + b + c);
 };
