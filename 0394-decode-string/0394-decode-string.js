6827d08f4628efd1c8d80d9d59b47da6c7124a8b
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-09-07 10:36:58 +0530

    Time: 0 ms (100%), Space: 54.4 MB (11.99%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -3,39 +3,24 @@
  * @return {string}
  */
 var decodeString = function (s) {
-    let lastBoxedStringFreq = 0;
-    let lastBoxedString = '';
-    let isBracketOpenCount = 0;
-    let res = '';
+    let stack = [];
+    let currNum = 0;
+    let currStr = '';
 
     for (let i = 0; i < s.length; i++) {
         if (s[i] >= '0' && s[i] <= '9') {
-            if (isBracketOpenCount) {
-                lastBoxedString += s[i];
-            } else {
-                lastBoxedStringFreq += s[i];
-            }
-        } else if (s[i] >= 'a' && s[i] <= 'z') {
-            if (isBracketOpenCount) {
-                lastBoxedString += s[i];
-            } else {
-                res += s[i];
-            }
+            currNum = 10 * currNum + Number(s[i]);
+        } else if (s[i] == '[') {
+            stack.push([currStr, currNum]);
+            currStr = '';
+            currNum = 0;
         } else if (s[i] == ']') {
-            isBracketOpenCount--;
-            // if valid
-            if (!isBracketOpenCount) {
-                res += decodeString(lastBoxedString.slice(1)).repeat(+lastBoxedStringFreq);
-                lastBoxedStringFreq = 0;
-                lastBoxedString = '';
-            } else {
-                lastBoxedString += s[i];
-            }
+            let [prevStr, num] = stack.pop();
+            currStr = prevStr + currStr.repeat(num);
         } else {
-            isBracketOpenCount++;
-            lastBoxedString += s[i];
+            currStr += s[i];
         }
     }
 
-    return res;
+    return currStr;
 };
