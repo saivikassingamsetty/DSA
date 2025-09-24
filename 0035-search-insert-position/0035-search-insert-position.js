29ec81898473dc8532bfa72664f39f07a5f89693
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-09-24 23:30:58 +0530

    Time: 0 ms (100%), Space: 54.2 MB (36.93%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -8,12 +8,11 @@ var searchInsert = function (nums, target) {
     let r = nums.length - 1;
 
     while (l < r) {
-        let mid = l + Math.floor((r - l) / 2);
-        if (nums[mid] == target) return mid;
-        else if (nums[mid] < target) l = mid + 1;
-        else r = mid;
+        let mid = Math.floor((l + r) / 2);
+        if (nums[mid] == target) return mid
+        else if (nums[mid] < target) l = mid + 1
+        else r = mid
     }
 
-    //this is where I lost, this is post processing -> checking last element
-    return (nums[l] < target) ? l + 1 : l;
+    return nums[l] < target ? l + 1 : l;
 };
