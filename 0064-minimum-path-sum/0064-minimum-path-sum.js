8301d0f9cd0e47064cef824172f34cc8e2bb701d
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-11-06 22:54:43 +0530

    Time: 7 ms (44.44%), Space: 56.7 MB (67.12%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -4,16 +4,16 @@
  */
 var minPathSum = function (grid) {
     const [m, n] = [grid.length, grid[0].length];
-    const dp = Array.from({ length: m }, () => new Array(n).fill(Infinity));
+    const dp = new Array(n).fill(Infinity);
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (i == 0 && j == 0) dp[i][j] = grid[0][0]
-            else if (i == 0) dp[i][j] = grid[i][j] + dp[i][j - 1]
-            else if (j == 0) dp[i][j] = grid[i][j] + dp[i - 1][j]
-            else dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1])
+            if (i == 0 && j == 0) dp[j] = grid[0][0];
+            else if (i == 0) dp[j] = grid[i][j] + dp[j - 1];
+            else if (j == 0) dp[j] += grid[i][j];
+            else dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1])
         }
     }
 
-    return dp[m - 1][n - 1];
+    return dp[n - 1];
 };
