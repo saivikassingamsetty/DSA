13d22c72a7b6686aab3b108d0203fe8a0dbd6cc7
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2026-08-18 00:19:18 +0530

    Time: 23 ms (67.4%), Space: 19.9 MB (45.16%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -3,22 +3,17 @@ class Solution:
         m, n = len(grid), len(grid[0])
 
         heap = [(grid[0][0], 0, 0)]
-        vis = set()
+        vis = {(0, 0)}
 
         while heap:
             time, i, j = heappop(heap)
 
             if i == m - 1 and j == n - 1:
                 return time
-            
-            if (i,j) in vis:
-                continue
-            
-            vis.add((i, j))
 
             for ni, nj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-                if (0 <= ni < m and 0 <= nj < n):
-                    newTime = max(time, grid[ni][nj])
-                    heappush(heap, (newTime, ni, nj))
+                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in vis:
+                    heappush(heap, (max(time, grid[ni][nj]), ni, nj))  # tracking the max height in the path, and starts the relaxation, this is the actual cost of reaching the neighbour
+                    vis.add((ni, nj))
         
         return -1
