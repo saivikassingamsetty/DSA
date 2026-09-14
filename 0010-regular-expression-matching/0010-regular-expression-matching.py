ea0db920f540e6153d7f6c03c98f1cb5d6b3a418
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2026-09-14 17:50:47 +0530

    Time: 7317 ms (7.1%), Space: 19.4 MB (68.82%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n, m = len(s), len(p)
+
+        def solve(i, j):
+            # if both reached end
+            if j == m:
+                return i == n
+
+            # if char is . or char
+            first_match = i < n and (s[i] == p[j] or p[j] == ".")
+
+            # if next char is *
+            if j + 1 < m and p[j + 1] == "*":
+                skip = solve(i, j + 2)
+                take = first_match and solve(i + 1, j) # first_match should be at first to skip the short circuit and going in a recursive call
+                return skip or take
+
+            # if match, check next
+            return first_match and solve(i + 1, j + 1)
+
+        return solve(0, 0)
